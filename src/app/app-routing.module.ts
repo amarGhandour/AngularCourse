9966ec3c648f15f5cd20ac6919ee00b04cc55e5c
mora <amarghandour89@gmail.com>
2022-07-18 22:20:23 +0200

    Last Lab

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ProductsListComponent} from "./components/products-list/products-list.component";
+import {LoginComponent} from "./components/login/login.component";
+import {RegisterComponent} from "./components/register/register.component";
+import {NotFoundPageComponent} from "./components/not-found-page/not-found-page.component";
+import {MainLayoutComponent} from "./components/main-layout/main-layout.component";
+import {ProductDetailsComponent} from "./components/product-details/product-details.component";
+import {CartComponent} from "./components/cart/cart.component";
+import {AuthGuard} from "./Guards/auth.guard";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: MainLayoutComponent, children: [
+      {path: "products", component: ProductsListComponent},
+      {path: "", redirectTo: "products", pathMatch: "full"},
+      {path: "products/:id", component: ProductDetailsComponent},
+      {path: "cart", component: CartComponent, canActivate: [AuthGuard]},
+      {path: "login", component: LoginComponent},
+      {path: "register", component: RegisterComponent},
+    ]},
+  {path: "**", component: NotFoundPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
